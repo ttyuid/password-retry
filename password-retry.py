b85e9fb6e9b69a3e2fa735008886091d7d0e1da7
江庭萱 <trista199847@gmail.com>
2018-10-21 00:59:46 +0800

    final version

--- password-retry.py
+++ password-retry.py
@@ -1,11 +1,15 @@
 password = 'a123456'
 x = 3
 while x > 0:
+    x = x - 1
     pwd = input('請輸入密碼')
     if pwd == password:
         print('登入成功！')
         break
     else:
-        x = x - 1
-        print('密碼錯誤！ 還有', x, '次機會')
-
+        print('密碼錯誤！')
+        if x > 0:
+            print('還有', x, '次機會')
+        else:
+            print('帳號鎖住了！！！')
+            
